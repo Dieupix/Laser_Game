7063c0a9724edc5cf667b5fc4dabffeee1dc9a5b
Dieupix <79543909+Dieupix@users.noreply.github.com>
2021-11-10 19:36:12 +0100

    Classe laser : Définition du constructeur et du destructeur Implémentation du constructeur Documentation du constructeur et du destructeur

---
+++ Laser_Game/laser.cpp
@@ -0,0 +1,32 @@
+#include "laser.h"
+
+// ---------- Constructors ----------
+
+laser::laser(const point& p, int direction, double step) : objet{p}, this->direction{direction}, this->step{step} {}
+
+// ---------- End of constructors ----------
+
+// ---------- Destructor ----------
+
+// ---------- End of destructor ----------
+
+// ---------- Overloads ----------
+
+// ---------- End of overloads ----------
+
+// ---------- Getters ----------
+
+// ---------- End of getters ----------
+
+// ---------- Setters ----------
+
+// ---------- End of setters ----------
+
+// ---------- Functions ----------
+
+// ---------- End of functions ----------
+
+// ---------- Global functions ----------
+
+// ---------- End of global functions ----------
+
---
+++ Laser_Game/laser.h
@@ -0,0 +1,62 @@
+#ifndef LASER_H
+#define LASER_H
+
+#include "objet.h"
+
+
+class laser : public objet
+{
+    private:
+        /************************************************
+         * @brief The direction of the laser
+         * @see enum directions
+         ***********************************************/
+        short short int direction;
+        /************************************************
+         * @brief The step of the laser
+         ***********************************************/
+        double step;
+
+    public:
+
+        // Enumeration
+        /************************************************
+         * @brief Enumeration of the directions
+         *
+         * @details This enumeration defines the different directions
+         * the laser can have
+         ***********************************************/
+        enum directions{
+            LEFT, RIGHT, UP, DOWN;
+        };
+
+        // Constructors
+        /************************************************
+         * @brief Constructor with a position, a direction and a step
+         *
+         * @param p : The initial position of the laser
+         * @param direction : The initial direction of the laser
+         * @param step : The step of the laser
+         * @return Creates a laser
+         ***********************************************/
+        laser(const point&, int, double);
+
+        // Destructor
+        /************************************************
+         * @brief Default destructor
+         *
+         * @details Destroys the laser
+         ***********************************************/
+        virtual ~laser() = default;
+
+        // Overloads
+
+        // Functions
+        void changeDirection(short short int);
+        void move();
+
+}; // End of laser
+
+// Global functions
+
+#endif // LASER_H
