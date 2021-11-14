b26a6192d42bb1558cd09d34c269981f9f095bc0
Yep <ZedBackTV@gmail.com>
2021-11-14 12:56:30 +0100

    Ajout des operator de la classe point + fichier de la classe tireur

--- Laser_Game/point.cpp
+++ Laser_Game/point.cpp
@@ -43,3 +43,95 @@ void point::print(std::ostream& ost) const
 {
     ost<<'('<<d_x<<','<<d_y<<')';
 }
+
+std::ostream& operator<<(std::ostream& ost, const point& p){
+    p.print(ost);
+    return ost;
+}
+
+std::istream& operator>>(std::istream& ist, point& p){
+    p.read(ist);
+    return ist;
+}
+
+point operator*(double d, const point& p){
+    return p*d;
+}
+
+ bool point::operator==(const point& p) const{
+        return (d_x == p.x()) and (d_y == p.y());
+    }
+
+    bool point::operator!=(const point& p) const{
+        return (x() != p.x()) or (y() != p.y());
+    }
+
+    point point::operator=(const point& p) const{
+        return {p.x(), p.y()};
+    }
+
+    point& point::operator=(const point& p){
+        if(this != &p){
+            d_x = p.x();
+            d_y = p.y();
+        }
+        return *this;
+    }
+
+    point point::operator-() const{
+        double x = d_x == 0 ? 0 : -d_x;
+        double y = d_y == 0 ? 0 : -d_y;
+        return {x, y};
+    }
+
+    point point::operator+(const point& p) const{
+        point tmp{*this};
+        tmp += p;
+        return tmp;
+    }
+
+    point& point::operator+=(const point& p) {
+        d_x += p.x();
+        d_y += p.y();
+        return *this;
+    }
+
+    point point::operator-(const point& p) const{
+        point tmp{*this};
+        tmp -= p;
+        return tmp;
+    }
+
+    point& point::operator-=(const point& p){
+        d_x -= p.x();
+        d_y -= p.y();
+        return *this;
+    }
+
+    point point::operator*(double d) const{
+        point tmp{*this};
+        tmp *= d;
+        return tmp;
+    }
+
+    point& point::operator*=(double d){
+        if(d != 1){
+            d_x *= d;
+            d_y *= d;
+        }
+        return *this;
+    }
+
+    point point::operator/(double d) const{
+        point tmp{*this};
+        tmp /= d;
+        return tmp;
+    }
+
+    point& point::operator/=(double d){
+        if(d != 1){
+            d_x = d_x / d;
+            d_y = d_y / d;
+        }
+        return *this;
+    }
--- Laser_Game/point.h
+++ Laser_Game/point.h
@@ -7,32 +7,36 @@ class point
 {
     public:
         /**
-        * @brief Constructeur d'un point avec les coordonnees x et y en parametres
-        *
-        * @param x : Coordonnee de x
-        * @param y : Coordonnee de y
-        *
-        * @return Créer un point
-        */
-        point(double x, double y);
-        /**
         * @brief Constructeur d'un point par defaut sans parametre
         *
         * @return Creer un point en x = 0.0 et y = 0.0
         */
         point();
+
+        /**
+        * @brief Constructeur d'un point avec les coordonnees x et y en parametres
+        *
+        * @param x : Coordonnee de x
+        * @param y : Coordonnee de y
+        *
+        * @return Creer un point
+        */
+        point(double x, double y);
+
         /**
         * @brief Fonction qui retourne la coordonee du point en x
         *
         * @return La coordonnee x du point
         */
         double x() const;
+
         /**
         * @brief Fonction qui retourne la coordonee du point en y
         *
         * @return La coordonnee y du point
         */
         double y() const;
+
         /**
         * @brief Procedure qui deplace un point aux cordonnees x et y en parametre
         *
@@ -42,6 +46,7 @@ class point
         * @return Déplace un point aux cordonnees (x,y)
         */
         void move(double x, double y);
+
         /**
         * @brief Procédure qui translate un point grace aux coordonnees (x,y) en parametres
         *
@@ -51,6 +56,7 @@ class point
         * @return Translate un point grace aux cordonnees (x,y)
         */
         void shift(double x, double y);
+
         /**
         * @brief Permet de lire un flux
         *
@@ -59,6 +65,7 @@ class point
         * @return void
         */
         void print(std::ostream& ost) const;
+
         /**
         * @brief Permet d'écrire sur un flux
         *
@@ -67,8 +74,138 @@ class point
         * @return void
         */
         void read(std::istream& ist);
+
+        /** @brief Permet de tester l'égaliter de 2 points
+         *
+         * @param p const point&
+         * @return un bool (vrai/faux)
+         *
+         */
+        bool operator==(const point& p) const;
+
+        /** @brief Permet de tester la difference de 2 points
+         *
+         * @param p const point&
+         * @return un bool (vrai/faux)
+         *
+         */
+        bool operator!=(const point& p) const;
+
+        /** @brief Permet l'affection d'un point
+         *
+         * @param p const point&
+         * @return point
+         *
+         */
+        point operator=(const point& p) const;
+
+        /** @brief Permet l'affection d'un point
+         *
+         * @param p const point&
+         * @return point&
+         *
+         */
+        point& operator=(const point& p);
+
+        /** @brief Permet la soustraction
+         *
+         * @return point
+         *
+         */
+        point operator-() const;
+
+        /** @brief Permet l'addition
+         *
+         * @param p const point&
+         * @return point
+         *
+         */
+        point operator+(const point& p) const;
+
+        /** @brief Permet l'addition et l'affectation
+         *
+         * @param p const point&
+         * @return point&
+         *
+         */
+        point& operator+=(const point& p);
+
+        /** @brief Permet la soustraction de deux points
+         *
+         * @param p const point&
+         * @return point
+         *
+         */
+        point operator-(const point& p) const;
+
+        /** @brief Permet la soustraction et l'affectation
+         *
+         * @param p const point&
+         * @return point&
+         *
+         */
+        point& operator-=(const point& p);
+
+        /** @brief Permet la multiplication par un coefficient reel
+         *
+         * @param d double
+         * @return point
+         *
+         */
+        point operator*(double d) const;
+
+        /** @brief Permet la multiplication et l'affectation
+         *
+         * @param d double
+         * @return point&
+         *
+         */
+        point& operator*=(double d);
+
+        /** @brief Permet la division par un reel
+         *
+         * @param d double
+         * @return point
+         *
+         */
+        point operator/(double d) const;
+
+        /** @brief Permet la division et l'affectation
+         *
+         * @param d double
+         * @return point&
+         *
+         */
+    point& operator/=(double d);
     private:
         double d_x, d_y;
 };
+
+        /** @brief Permet d'imprimer un point
+         *
+         * @param ost std::ostream&
+         * @param p const point&
+         * @return le flux d'un point
+         *
+         */
+        std::ostream& operator<<(std::ostream& ost, const point & p);
+
+        /** @brief Permet de lire un point
+         *
+         * @param ost std::ostream&
+         * @param p const point&
+         * @return le flux d'un point
+         *
+         */
+        std::istream& operator>>(std::istream& ist, point& p);
+
+        /** @brief Permet de multiplier un point par un reel
+         *
+         * @param d double
+         * @param p const point&
+         * @return point
+         *
+         */
+        point operator*(double d, const point& p);
 
 #endif // POINT_H
