898f4ab8ba625515e11e5aaf0c5820dd9daf5764
Mathis CHARRIER <mathischarrier7@gmail.com>
2021-11-30 16:54:26 +0100

    Ajout de la documentation de la classe tireur

--- Laser_Game/tireur.h
+++ Laser_Game/tireur.h
@@ -6,16 +6,38 @@
 class tireur : public object
 {
     public:
+         /** @brief L'enumeration des directions disponibles pour le tireur
+          */
          enum directions : short unsigned int{
             RIGHT,
             UP,
             LEFT,
             DOWN
         };
+
+        /** @brief Constructeur du tireur
+         *
+         * @param p const point& une position
+         * @param directions une direction de l'énumération de directions
+         *
+         */
         tireur(const point &p, directions);
+
+        /** @brief Destructeur virtuel de la classe
+         */
         virtual ~tireur() = default;
+
+
+        /** @brief Permet au tireur de tirer
+         *
+         * @param un double qui determine le pas du tir
+         * @return le laser
+         *
+         */
         laser tire(double step);
     private:
+        /** @brief la direction du tireur
+         */
         directions direction;
 
 };
