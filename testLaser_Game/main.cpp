88c33d98833a87de9de75d713c127e29bda3ea91
Mathis CHARRIER <83589874+MathisCharrier@users.noreply.github.com>
2022-01-04 13:29:43 +0100

    Ajout des tests de diverses class

--- testLaser_Game/main.cpp
+++ testLaser_Game/main.cpp
@@ -6,7 +6,7 @@ int main() {
 
     context.setOption("order-by", "name");  // sort the test cases by their name
     //context.setOption("duration", true);    // print the duration of each test case
-    //context.setOption("success", true);     // print all tests
+    context.setOption("success", true);     // print all tests
 
     return context.run();                   // run
 }
