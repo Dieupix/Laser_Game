855145fc9f1860d845b7b30b24ffa4b7a6f0ffe2
Elnuma68 <manuelbarrere8@gmail.com>
2022-01-05 19:29:22 +0100

    Mise en forme de lasergame

--- Laser_Game/lasergame.h
+++ Laser_Game/lasergame.h
@@ -1,17 +1,63 @@
 #ifndef LASERGAME_H
 #define LASERGAME_H
+
+//Declaring libraries
 #include "game.h"
+
+//Declaring namespace elements
 using std::cout;
 using std::endl;
+
+/**
+ * @class lasergame lasergame.h
+ * @brief A class that represents a laser game
+ * @details This class represents a laser game, with a @a viewer
+*/
 class lasergame
 {
     public:
+        /**
+        * @brief Constructor by default of lasergame (by default viewerOnTerminal)
+        *
+        * @return Create a lasergame with a viewer (by default viewerOnTerminal)
+        */
         lasergame();
+
+        /**
+        * @brief Virtual destructor by default of lasergame
+        */
         virtual ~lasergame() = default;
+
+        /**
+        * @brief Procedure that allows to print a menu
+        *
+        * @return void
+        */
         void menu();
-        void typeGraphique();
-        void afficheTerrain(const string& path);
-        string choisirTerrain();
+
+        /**
+        * @brief Procedure that allows to make a choice between different types of viewers
+        *
+        * @return void
+        */
+        void GraphicType();
+
+        /**
+        * @brief Procedure that allows to print grounds of a game (in a file or repertory)
+        *
+        * @param path : the name of the file (const string)
+        *
+        * @return void
+        */
+        void printGround(const string& path);
+
+        /**
+        * @brief Function that allows to make a choice between different grounds
+        *
+        * @return the path of the ground
+        */
+        string Ground_choice();
+
     private:
         unique_ptr<viewer> d_viewer;
 };
